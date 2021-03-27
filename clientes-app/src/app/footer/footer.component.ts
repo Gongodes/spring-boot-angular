bc9761421bd5fc1c29429a6714a240b16f02f5a1
Gonzalo Godoy Espinosa <gonzalo.godoy.esp@gmail.com>
2021-03-27 19:05:30 -0300

    test

--- clientes-app/src/app/footer/footer.component.ts
+++ clientes-app/src/app/footer/footer.component.ts
@@ -6,5 +6,5 @@ templateUrl: './footer.component.html',
 styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-  public autor: any = {nombre:'Gonzalo', apellido: 'Godoy'};
+  public autor: any = {nombre:'Andrés', apellido: 'Guzmán'};
 }
